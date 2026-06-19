962d93db11f5fd3bd7df08fc669a0e69fcd3171d
Vitor Almeida <vitor.almeida@serpro.gov.br>
2026-06-19 16:52:20 -0300

    fix: corrigir erro de inicialização das notificações no Android
    - Atualizada a referência do ícone de notificação para '@mipmap/launcher_icon' no NotificationService, alinhando com a configuração do launcher_icon.
    - Regenerados os ícones de launcher para todas as plataformas usando flutter_launcher_icons.

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -13,7 +13,7 @@ class NotificationService {
 
   Future<void> init() async {
     const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
+        AndroidInitializationSettings('@mipmap/launcher_icon');
 
     const DarwinInitializationSettings initializationSettingsDarwin =
         DarwinInitializationSettings(
